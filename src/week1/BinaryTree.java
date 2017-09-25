ea559fb7d7f8d43bc318ffc4d6e4158e8dac9f0c
Johannes Sebastian Lade <johannes.lade@yahoo.com>
2017-09-25 13:32:26 +0200

    Deleted Quicksort

--- src/week1/BinaryTree.java
+++ src/week1/BinaryTree.java
@@ -1,51 +1,38 @@
 package week1;
 
+import utils.IO;
 
 public class BinaryTree {
 
 
     public static void main(String[] args) {
         BinaryTree tree = new BinaryTree();
-        double[] a = new double[]{5, 1, 4, 3, 4};
-        tree.quicksort(a, 0, a.length-1, 1);
-        tree.printArray(a);
+        double[][] a = new double[][]{{5, 5}, {1, 4}, {4, 3}, {3, 2}, {4, 1}};
+        tree.quicksort(a, 0, a.length-1);
+        IO.print(a);
+        IO.print("hello");
     }
 
-    public void quicksort(double[] data, int lo, int hi, double dim) {
-        if (lo < hi) {
-            double piv = data[lo];
-            int part = partition(data, lo, hi, piv, dim);
-            quicksort(data, lo, part, dim);
-            quicksort(data, part+1, hi, dim);
-        }
-    }
-
-    public int partition(double[] data, int lo, int hi, double pivot, double dimension) {
+    private int partition(double[][] data, int lo, int hi, double pivot, int dimension) {
         int i = lo - 1;
         int j = hi + 1;
+        int index = dimension-1;
         while (true) {
-            while (data[++i] < pivot);
-            while (data[--j] > pivot);
+            while (data[++i][index] < pivot && i < j);
+            while (data[--j][index] > pivot && j > i);
             if (i >= j) {
                 return j;
             }
-            swap(data, i, j);
+            swap(data, i, j, index);
         }
     }
 
-
-    private void swap(double[] a, int i, int j) {
-        double tmp = a[i];
+    private void swap(double[][] a, int i, int j, int index) {
+        double[] tmp = a[i];
         a[i] = a[j];
         a[j] = tmp;
     }
 
-    private void printArray(double[] a) {
-        System.out.print("[");
-        for (int i=0; i<a.length; i++) {
-            System.out.print(a[i] + ",");
-        }
-        System.out.println("]");
-    }
+
 }
 
