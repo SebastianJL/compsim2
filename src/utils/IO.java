dc73a903700952108e3871f42151e8c175ced183
Johannes Sebastian Lade <johannes.lade@yahoo.com>
2017-10-02 12:36:41 +0200

    method overloads for more arguments

--- src/utils/IO.java
+++ src/utils/IO.java
@@ -7,23 +7,33 @@ public class IO {
 
     public static <T> void print(T[] array) { System.out.println(toString(array));}
 
-    private static <T> String toString(T[] array) {
+
+    public static <T> String toString(T[] array) {
+        return toString(array, 0, array.length-1);
+    }
+
+    public static <T> String toString(T[] array, int start, int end) {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for (int i=0; i<array.length-1; i++) {
+        for (int i=start; i<end; i++) {
             sb.append(array[i]).append(",\n");
         }
-        sb.append(array[array.length-1]).append("]");
+        sb.append(array[end]).append("]");
         return sb.toString();
     }
 
-    public static String toString(double[] a) {
+
+    public static String toString(double[] array) {
+        return toString(array, 0, array.length-1);
+    }
+
+    public static String toString(double[] a, int start, int end) {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for (int i=0; i<a.length-1; i++) {
+        for (int i=start; i<end; i++) {
             sb.append(a[i]).append(", ");
         }
-        sb.append(a[a.length-1]).append("]");
+        sb.append(a[end]).append("]");
         return sb.toString();
     }
 }
