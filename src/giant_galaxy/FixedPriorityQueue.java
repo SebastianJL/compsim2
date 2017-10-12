1e8e41cce120b488ff5357739f5987905eb0a6a2
Johannes Sebastian Lade <johannes.lade@yahoo.com>
2017-10-12 13:06:17 +0200

    Implemented FixedPriorityQueue

---
+++ src/giant_galaxy/FixedPriorityQueue.java
@@ -0,0 +1,10 @@
+package giant_galaxy;
+
+public interface FixedPriorityQueue {
+
+    void insert(double r, int index);
+    double max();
+//    int delNext();
+//    boolean isEmpty();
+//    int size();
+}
---
+++ src/giant_galaxy/LinearFixedPriorityQueue.java
@@ -0,0 +1,49 @@
+package giant_galaxy;
+
+
+/**
+ * Fixed width priority queue that keeps n elements with the highest priority. I.e. elements with lower priority are
+ * kicked out.
+ */
+public class LinearFixedPriorityQueue implements FixedPriorityQueue {
+    int[] indices;
+    double[] r2List;
+    int r2Max; // Index of maximal r2 in r2List.
+
+    LinearFixedPriorityQueue(int size) {
+        indices = new int[size];
+        r2List = new double[size];
+        r2Max = 0;
+
+        for (int i = 0; i < indices.length; i++) {
+            indices[i] = -1;
+            r2List[i] = Double.POSITIVE_INFINITY;
+        }
+    }
+
+    @Override
+    public void insert(double r2, int index) {
+        if (r2 < r2List[r2Max]) {
+            r2List[r2Max] = r2;
+            indices[r2Max] = index;
+
+            // Linear search for new r2Max;
+            double max = 0;
+            for (int i = 0; i < r2List.length; i++) {
+                if (r2List[i] > max) {
+                    max = r2List[i];
+                    r2Max = i;
+                }
+            }
+        }
+    }
+
+    @Override
+    public double max() {
+        return r2List[r2Max];
+    }
+
+    int[] particles() {
+        return indices.clone();
+    }
+}
